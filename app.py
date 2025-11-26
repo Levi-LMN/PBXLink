a1c8f267de691b7abaabb2ee734959b938c4fbe5
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2025-11-26 17:14:11 +0300

    updated env

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def create_app(config_name=None):
 if __name__ == '__main__':
     app = create_app()
     app.run(
-        host='0.0.0.0',
+        host='10.200.200.1',  # Bind specifically to WireGuard IP
         port=5000,
         debug=app.config['DEBUG']
     )
