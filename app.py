aa66f02d66acd5f0ce84e2e0c23f54c688659e09
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2025-11-26 17:16:27 +0300

    updated hostnames

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def create_app(config_name=None):
 if __name__ == '__main__':
     app = create_app()
     app.run(
-        host='10.200.200.1',  # Bind specifically to WireGuard IP
+        host='0.0.0.0',
         port=5000,
         debug=app.config['DEBUG']
     )
